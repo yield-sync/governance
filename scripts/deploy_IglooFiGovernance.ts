dd829f97be3bb36210a50d67f6767a9275027b88
harpoonjs <w3st.io2021@gmail.com>
2023-03-28 21:46:48 -0400

    Edit

--- scripts/deploy_IglooFiGovernance.ts
+++ scripts/deploy_IglooFiGovernance.ts
@@ -1,17 +1,22 @@
-const { ethers } = require('hardhat');
+require("dotenv").config();
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+
 
 async function main() {
 	const [deployer] = await ethers.getSigners();
 
 	console.log(`Deploying contracts with the account: ${deployer.address}`);
 
-	const yieldSyncGovernance = await (await ethers.getContractFactory('YieldSyncGovernance')).deploy();
+	const yieldSyncGovernance: Contract = await (await ethers.getContractFactory('YieldSyncGovernance')).deploy();
 
 	console.log(`Contract address: ${yieldSyncGovernance.address}`);
 }
 
 main()
 	.then(() => {
+		console.log(process.env)
+
 		process.exit(0);
 	})
 	.catch((error) => {
